2c9b8087183c4e3c3f9d688087faf9ba545d2037
soubhagya <soubhagya.pradhan@fractal.ai>
2019-08-03 22:04:12 +0530

    addign stripe

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import CollectionPreview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import SHOP_DATA from './shop.data.js';
+const ShopPage = ({ match }) =>  (
+    <div className='shop-page'>
+      <Route exact path={`${match.path}`} component={CollectionPreview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+)
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+export default ShopPage
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
-const middlewires = [logger];
-const store = createStore(rootReducer, applyMiddleware (...middlewires))
+export const middlewires = [logger];
+export const store = createStore(rootReducer, applyMiddleware (...middlewires))
+export const  persistor = persistStore(store);
 
-export default store;
+export default {store, persistor};
